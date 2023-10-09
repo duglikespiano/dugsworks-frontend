5c2e749b9ed966e9d5c3b8c34382dec23d839509
SUHO PARK <duglikespiano@gmail.com>
2023-10-09 14:39:00 +0900

    REFACTOR : LANGUAGE SELECTOR LOGIC REFACTORED

--- src/components/ja/shared/LanguageSelectorJa/LanguageSelectorJa.tsx
+++ src/components/ja/shared/LanguageSelectorJa/LanguageSelectorJa.tsx
@@ -1,19 +1,47 @@
 import { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './LanguageSelectorJa.module.scss';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { languages } from '../../../../variables';
+import styles from './LanguageSelectorJa.module.scss';
 
 export default function LanguageSelectorJa() {
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const navigateByLanguage = (language: string) => {
+		let newURL = '';
+		let URLArray = location.pathname.split('/');
+		if (language === '한글') {
+			URLArray[1] = 'ko';
+		} else if (language === '日本語') {
+			URLArray[1] = 'ja';
+		} else {
+			URLArray[1] = 'en';
+		}
+		URLArray.slice(0, 1);
+		newURL = URLArray.join('/');
+		navigate(newURL);
+	};
+
 	return (
 		<Fragment>
 			<div className={styles['language-selector-box']}>
-				<Link className={styles['language-selector']} to={'/ko'}>
+				<span
+					className={styles['language-selector']}
+					onClick={() => {
+						navigateByLanguage(languages[1].language);
+					}}
+				>
 					{languages[1].language}
-				</Link>
+				</span>
 				/
-				<Link className={styles['language-selector']} to={'/en'}>
+				<span
+					className={styles['language-selector']}
+					onClick={() => {
+						navigateByLanguage(languages[0].language);
+					}}
+				>
 					{languages[0].language}
-				</Link>
+				</span>
 			</div>
 		</Fragment>
 	);
