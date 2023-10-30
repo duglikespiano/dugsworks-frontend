5474c7fc9a438224b47774cf2ca2fd0235b2984b
SUHO PARK <duglikespiano@gmail.com>
2023-10-30 23:12:23 +0900

    FEATURE : THE NUMBER OF PROJECTS COUNTING FEATURE ADDED

--- src/components/en/ProjectsEn/ProjectsEn.tsx
+++ src/components/en/ProjectsEn/ProjectsEn.tsx
@@ -1,14 +1,25 @@
 import { Fragment } from 'react';
 import ProjectsKeywordsEn from './ProjectsKeywordsEn/ProjectsKeywordsEn';
 import ProjectsListEn from './ProjectsListEn/ProjectsListEn';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../sliceStore';
 import styles from './ProjectsEn.module.scss';
 
 export default function ProjectsEn() {
+	const projectsList = useSelector((state: RootState) => state.projectsList);
+	const selectedProjectKeywordsIncludeProjects = useSelector((state: RootState) => state.selectedProjectKeywordsIncludeProjects);
 	return (
 		<Fragment>
 			<div className={styles['projects-box']}>
 				<div className={styles['skills-filter-title']}>Skills filter</div>
 				<div className={styles['projects-skills-filter-description']}>Click skills to filter projects</div>
+				<div className={styles['projects-filtered-description']}>
+					<span className={styles['projects-filtered-description-emphasize-text']}>{projectsList.length}</span> of{' '}
+					<span className={styles['projects-filtered-description-emphasize-text']}>
+						{selectedProjectKeywordsIncludeProjects.selectedKeywordsIncludeProjects.length}
+					</span>{' '}
+					Project(s) selected
+				</div>
 				<ProjectsKeywordsEn />
 				<hr className={styles['horizontalrule']} />
 				<div className={styles['projects-title']}>Projects</div>
