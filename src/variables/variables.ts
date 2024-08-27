f39358a4abdd1c9ac7bf02bcd1f01a6985764ec2
SUHO PARK <duglikespiano@gmail.com>
2024-08-27 14:09:11 +0900

    REFATOR : SKILLSETS LAYOUT REFACTORED

--- src/variables/variables.ts
+++ src/variables/variables.ts
@@ -164,7 +164,7 @@ export const schoolHistory: HistoryType[] = [
 	},
 ];
 
-export const frontEndSkillSets = ['HTML/CSS', 'Javascript/Typescript', 'React', 'Redux', 'Vue', 'Sass'];
-export const backEndSkillSets = ['Node.js', 'Javascript/Typescript', 'Express', 'Python', 'MySQL', 'Socket.io'];
+export const frontEndSkillSets = ['HTML/CSS', 'Javascript', 'Typescript', 'React', 'Redux', 'Vue', 'Sass'];
+export const backEndSkillSets = ['Node.js', 'Javascript', 'Typescript', 'Express', 'Python', 'MySQL', 'Socket.io'];
 export const cloudSkillSets = ['AWS', 'Netlify', 'CloudType', 'Deployment'];
 export const miscSkillSets = ['Git', 'Linux', 'Vitest'];
